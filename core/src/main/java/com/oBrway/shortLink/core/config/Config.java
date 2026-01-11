c71dc07076a8922646d76fea0a22e75a8cb393e8
XiuWei-Yang <2226515496@stu.xjtu.edu.cn>
2026-01-11 18:23:16 +0800

    config

--- core/src/main/java/com/oBrway/shortLink/core/config/Config.java
+++ core/src/main/java/com/oBrway/shortLink/core/config/Config.java
@@ -4,6 +4,12 @@ import lombok.Data;
 
 @Data
 public class Config {
+    private String serviceId = "short-link-service";
+
+    private String registryAddress = "localhost:8888";
+
+    private String env = "dev";
+
     private static final long shortLink_length = 7;
     //短链接有效时间
     private static final long shortLink_validTime = 60 * 60 * 24 * 30; // 30天
